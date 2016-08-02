20ce3e86af6bf49a1b2abebc7c6721777cb15a43
Almino Melo <almino.melo@gmail.com>
2016-08-02 19:29:44 -0300

    Documentação

--- Traits/Href.php
+++ Traits/Href.php
@@ -4,6 +4,10 @@ namespace Onimla\HTML\Traits;
 
 trait Href {
 
+    /**
+     * @param string $url
+     * @return self
+     */
     public function href($url = FALSE) {
         return $this->attr(__FUNCTION__, $url);
     }
