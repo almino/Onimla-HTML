d617ed1b8fe8911941075a9ea5d0dfe6bb67342e
Almino Melo <almino.melo@gmail.com>
2016-08-23 17:16:25 -0300

    Obriga classes a ter métodos relacionados ao elemento `input`

---
+++ Polymorphism/Input.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Onimla\HTML\Polymorphism;
+
+interface Input {
+
+    public function setDisabled();
+
+    public function unsetDisabled();
+
+    public function disabled();
+
+    public function disable();
+
+    public function enable();
+
+    public function id($value = FALSE);
+
+    public function isValueSet();
+
+    public function name($value = FALSE);
+
+    public function readOnly();
+
+    public function isReadOnly();
+
+    public function isNotReadOnly();
+
+    public function setReadOnly();
+
+    public function unsetReadOnly();
+
+    public function required();
+
+    public function isRequired();
+
+    public function isNotRequired();
+
+    public function setRequired();
+
+    public function unsetRequired();
+
+    public function type($value = FALSE);
+
+    public function value($value = FALSE);
+}
