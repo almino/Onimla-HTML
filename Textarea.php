2d1821afefd567d654e54862f004bfd3d1e4dff9
Almino Melo <almino.melo@gmail.com>
2016-08-23 17:30:46 -0300

    Adota a interface `userInput` e implementa seus métodos.

--- Textarea.php
+++ Textarea.php
@@ -2,8 +2,10 @@
 
 namespace Onimla\HTML;
 
-class Textarea extends Element {
-    
+use Onimla\HTML\Polymorphism\UserInput;
+
+class Textarea extends Element implements UserInput {
+
     use Traits\Name;
 
     public function __construct($name, $value = FALSE, $rows = FALSE, $attr = NULL, $children = FALSE) {
@@ -15,15 +17,17 @@ class Textarea extends Element {
     }
 
     public function disabled() {
-        return (bool) $this->attr(__FUNCTION__);
+        return $this->isDisabled();
     }
-    
+
     public function disable() {
-        $this->attr('disabled', 'disabled');
+        $this->setDisabled();
+        return $this;
     }
 
     public function enable() {
-        return $this->removeAttr('disabled');
+        $this->unsetDisabled();
+        return $this;
     }
 
     public function rows($value = FALSE) {
@@ -38,6 +42,70 @@ class Textarea extends Element {
      */
     public function value($value = FALSE) {
         return $this->text(...func_get_args());
+    }
+
+    public function isDisabled() {
+        return (bool) $this->attr(__FUNCTION__);
+    }
+
+    public function isNotReadOnly() {
+        return !$this->isReadOnly();
+    }
+
+    public function isNotRequired() {
+        return !$this->isRequired();
+    }
+
+    public function isReadOnly() {
+        return (bool) $this->attr('readonly');
+    }
+
+    public function isRequired() {
+        return (bool) $this->attr('required');
+    }
+
+    public function isValueSet() {
+        return $this->getAttribute('value')->isValueSet();
+    }
+
+    public function readOnly() {
+        return $this->isReadOnly();
+    }
+
+    public function required() {
+        return $this->isRequired();
+    }
+
+    public function setDisabled() {
+        $this->attr('disabled', 'disabled');
+    }
+
+    public function setReadOnly() {
+        $this->attr('readonly', 'readonly');
+    }
+
+    public function setRequired() {
+        $this->attr('required', 'required');
+    }
+
+    public function type($value = FALSE) {
+        if ($value === FALSE) {
+            return 'textarea';
+        }
+
+        return $this;
+    }
+
+    public function unsetDisabled() {
+        $this->removeAttr('disabled');
+    }
+
+    public function unsetReadOnly() {
+        $this->removeAttr('readonly');
+    }
+
+    public function unsetRequired() {
+        $this->removeAttr('required');
     }
 
 }
